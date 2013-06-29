c05c63e733d7a1c9d965d8ebabb184a35d72439e
Oshani <oshanis@gmail.com>
2013-06-29 01:42:16 -0400

    added a grid view example

--- src/com/rationmatch/RationMatch.java
+++ src/com/rationmatch/RationMatch.java
@@ -2,7 +2,10 @@ package com.rationmatch;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 
 public class RationMatch extends Activity {
 
@@ -17,6 +20,18 @@ public class RationMatch extends Activity {
     // Inflate the menu; this adds items to the action bar if it is present.
     getMenuInflater().inflate(R.menu.ration_match, menu);
     return true;
+  }
+  
+  /**
+   * On click event for a button on the main rationview
+   */
+  public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+      // Sending image id to FullScreenActivity
+      Intent i = new Intent(getApplicationContext(), RationMenu.class);
+      // passing array index
+      //i.putExtra("id", position);
+      startActivity(i);
   }
 
 }
